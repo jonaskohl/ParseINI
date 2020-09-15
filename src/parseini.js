56b78011273510e1fd6153b5f8122271ed6c7b80
Crystal-RainSlide <16851802+Crystal-RainSlide@users.noreply.github.com>
2020-09-16 03:41:56 +0800

    Simplify the code & Fix a bug
    Simplify the code;
    Fix a bug in regex: /^\[*.+\]$/ -> /^\[.+\]$/

--- src/parseini.js
+++ src/parseini.js
@@ -1,25 +1,20 @@
 /**
- * ParseINI v1.0.0
+ * ParseINI v1.0.1
  * A simple, fast and lightweight INI file parser for JavaScript.
  * Written by and Copyright (c) 2018 Jonas Kohl (https://jonaskohl.de/)
  * https://github.com/jonaskohl/ParseINI
  */
-function ParseINI(content, resolveValueTypes) {
-  if (resolveValueTypes !== false)
-    resolveValueTypes = true;
+function ParseINI(content, resolveValueTypes = true) {
   function resolvetype(value) {
     if (value.trim().length < 1)
       return null;
-    else if (!isNaN(value)) {
-      if (value.indexOf(".") > -1)
-        return parseFloat(value, 10);
-      else
-        return parseInt(value, 10);
-    } else if (value.match(/^(true|on|enabled)$/i))
+    else if (!isNaN(value))
+      return value.indexOf(".") > -1 ? parseFloat(value, 10) : parseInt(value, 10);
+    else if (value.match(/^(true|on|enabled)$/i))
       return true;
     else if (value.match(/^(false|off|disabled)$/i))
       return false;
-    else if (value.match(/^null$/i))
+    else if (value === "null")
       return null;
     else
       return value;
@@ -28,19 +23,16 @@ function ParseINI(content, resolveValueTypes) {
   var currentKey = false;
   var lines = content.split(/(\r\n|\r|\n)/);
   for (var line of lines) {
-    if (line.match(/^\[*.+\]$/)) {
-      var key = line.replace(/^\[(.+)\]$/, "$1");
+    if (line.match(/^\[.+\]$/)) {
+      var key = line.slice(1, -1);
+      currentKey = key;
       if (!obj.hasOwnProperty(key))
         obj[key] = {};
-      currentKey = key;
     } else if (line.trim() !== "" && !line.startsWith(";")) {
       var kvp = line.split(/\s*=\s*/);
       var key = kvp[0];
       var value = kvp[1];
-      if (currentKey)
-        obj[currentKey][key] = resolveValueTypes ? resolvetype(value) : value;
-      else
-        obj[key] = resolveValueTypes ? resolvetype(value) : value;
+      (currentKey ? obj[currentKey] : obj)[key] = resolveValueTypes ? resolvetype(value) : value;
     }
   }
   return obj;
